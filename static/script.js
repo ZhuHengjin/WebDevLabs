99f312185b164bc1faa27b3da73eaad2c57a76e0
ZhuHengjin <HengjinZhu@iCloud.com>
2025-03-25 23:28:18 -0400

    new implementation for contact page

--- static/script.js
+++ static/script.js
@@ -1,64 +1,3 @@
-var x, y;
-x = 5;
-y = 7;
-var z = x + y;
-console.log(z);
-var A = "Hello ";
-var B = "world!";
-var C = A + B;
-console.log(C);
-
-function SumNPrint(x1, x2) {
-    var x3 = x1 + x2;
-    console.log(x3);
-}
-
-SumNPrint(x, y);
-SumNPrint(A, B);
-
-if (C.length > z) {
-    console.log(C);
-} else if (C.length < z) {
-    console.log(z);
-} else {
-    console.log("good job!");
-}
-
-L1 = ["Watermelon", "Pineapple", "Pear", "Banana"];
-L2 = ["Apple", "Banana", "Kiwi", "Orange"];
-
-// function findTheBanana(L) {
-//     for (var i = 0; i < L.length; i++) {
-//         if (L[i] == "Banana") {
-//             alert("Found the Banana at index " + i);
-//             break;
-//         }
-//     }
-// }
-
-// findTheBanana(L1);
-// findTheBanana(L2);
-
-// function checkSingleArray(singleArray, arrayIndex) {
-//     for (let i = 0; i < singleArray.length; i++) {
-//         if (singleArray[i] === "Banana") {
-//             if (arrayIndex === 0) {
-//                 alert("We found a banana in the first array");
-//             } else if (arrayIndex === 1) {
-//                 alert("We found a banana in the second array");
-//             } else {
-//                 alert("No banana found in first or second array");
-//             }
-//         }
-//     }
-// }
-
-// function findTheBananaForEach(arrays) {
-//     arrays.forEach(checkSingleArray);
-//   }
-
-// findTheBananaForEach([L1, L2]);
-
 function greetingFunc() {
     var d = new Date();
     var h = d.getHours();
@@ -136,3 +75,18 @@ function getAdvice() {
             console.error(error);
         });
 }
+
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const currentPath = window.location.pathname;
+  const navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach(link => {
+    if (link.pathname === currentPath) {
+      link.classList.add('active');
+    }
+  });
+});
